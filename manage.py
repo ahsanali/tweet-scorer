3f9d73d22a4c3ae150d3316d024b6e7369f1d0b5
Muhammad Ahsan ALi <sn.ahsanali@gmail.com>
2013-11-30 22:25:33 -0800

    comitting code

---
+++ manage.py
@@ -0,0 +1,26 @@
+#!flask/bin/python
+from app import app
+from flask.ext.script import Manager
+
+# app.run(debug = True, host='0.0.0.0', port=8001)
+
+
+manager = Manager(app)
+
+
+@manager.command
+def run():
+    """Run in local machine."""
+
+    app.run(host='0.0.0.0',port=8001,debug=True)
+
+
+
+
+manager.add_option('-c', '--config',
+                   dest="config",
+                   required=False,
+                   help="config file")
+
+if __name__ == "__main__":
+    manager.run()
