0b81deacf8055b07632cc6b39c25d44ddbf36efd
Ahmad Masood <ahmad.m.salahuddin@gmail.com>
2014-01-28 16:04:41 +0500

    Java Script added to sort buttons

---
+++ app/static/js/sort.js
@@ -0,0 +1,21 @@
+//This Functions sorts tweets according to parameters provided.
+function tweet_sort(sort_class){
+	
+	//Fetching All the divs whch 'media' class 
+	var myrow = new Array();
+	$(".media").each(function(data){
+		myrow[data] = this;
+	});
+
+	//Sorting Them according to provided call
+	myrow.sort(function(a,b){
+		return 	parseFloat(String($(a).find(sort_class).text()).trim()) - 
+				parseFloat(String($(b).find(sort_class).text()).trim())
+	});
+	
+	var newhtml=""
+	for(i=0 ; i < myrow.length ; i++){
+		newhtml = newhtml + "<div class='media'>"+$(myrow[i]).html()+"</div>";
+	}
+	$("#tweet-container").html(newhtml);
+}
